e53def08db37baa28d5a54560ee6e1a8b269a271
DJ Corbett <djcorbett96@gmail.com.com>
2024-01-02 13:52:00 -0600

    updating link tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,18 @@ const Index = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
       <div className="text-3xl mb-10 text-[#0a3366]">Select an experience:</div>
-      <a href="/harrypotter">
+      <Link href="/harrypotter">
         <div className="border rounded-lg py-3 w-96 text-center text-xl shadow text-[#0a3366] hover:bg-[#0a3366] hover:text-white transition ease-linear cursor-pointer">
           {' '}
           Harry Potter
         </div>
-      </a>
-      <a href="/hitchhikers">
+      </Link>
+      <Link href="/hitchhikers">
         <div className="border rounded-lg py-3 w-96 text-center text-xl shadow text-[#0a3366] hover:bg-[#0a3366] hover:text-white transition ease-linear cursor-pointer">
           {' '}
           Hitchhikers
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
